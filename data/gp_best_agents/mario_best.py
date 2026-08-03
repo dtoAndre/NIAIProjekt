59716ef071f64391911c7c4ab3a43e82b19fd71d
dtoAndre <asdsd.070@gmail.com>
2026-08-03 12:51:28 +0100

    Initial project commit

---
+++ data/gp_best_agents/mario_best.py
@@ -0,0 +1,10 @@
+
+# Evolved Mario Controller (Random Search)
+# Fitness: 0.0
+
+def corre(action, landscape, enemies, can_jump, on_ground, Mario, Sprite, **kwargs):
+    if on_ground:
+        if can_jump:
+            if can_jump:
+                if on_ground:
+                    pass
